cc9c6f453a02800122a4bd19677101ef4bdc66c5
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-04-12 23:06:23 +0530

    create Customer form user interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-
-public class Main{
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.print("hello World");
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/CustomerForm.fxml"))));
+        stage.getIcons().add(new Image("img/Logo.png"));
+        stage.setTitle("Thoga Kade");
+        stage.show();
     }
 }
---
+++ src/main/java/edu/icet/thogakade/controller/CustomerFormController.java
@@ -0,0 +1,65 @@
+package edu.icet.thogakade.controller;
+
+
+public class CustomerFormController{
+    public JFXButton btnItemForm;
+    @FXML
+    private Label lblCustomerCount;
+    @FXML
+    private JFXTextField txtCustomerId;
+    @FXML
+    private JFXTextField txtCustomerName;
+    @FXML
+    private DatePicker dpDate;
+    @FXML
+    private JFXTextField txtSalary;
+    @FXML
+    private JFXTextField txtAddress;
+    @FXML
+    private JFXTextField txtCity;
+    @FXML
+    private JFXTextField txtProvince;
+    @FXML
+    private JFXTextField txtPostalCode;
+    @FXML
+    private JFXComboBox cmbTitle;
+    @FXML
+    private TableView table1;
+    @FXML
+    private TableColumn colCustomerId1;
+    @FXML
+    private TableColumn colTitle;
+    @FXML
+    private TableColumn colName;
+    @FXML
+    private TableColumn colDOB;
+    @FXML
+    private TableColumn colSalary;
+    @FXML
+    private TableView table2;
+    @FXML
+    private TableColumn colCustomerId2;
+    @FXML
+    private TableColumn colAddress;
+    @FXML
+    private TableColumn colCity;
+    @FXML
+    private TableColumn colPostalCode;
+    @FXML
+    private TableColumn colProvince;
+
+
+    public void searchOnAction(ActionEvent actionEvent) { 
+    }
+
+    public void AddOnAction(ActionEvent actionEvent) {
+    }
+
+
+    public void UpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+}
