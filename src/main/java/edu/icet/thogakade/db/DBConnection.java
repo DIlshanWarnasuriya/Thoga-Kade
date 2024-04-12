68d0eb289610b471a4db567fc0c9205c766da379
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-04-12 23:32:44 +0530

    create database Connection

---
+++ src/main/java/edu/icet/thogakade/db/DBConnection.java
@@ -0,0 +1,26 @@
+package edu.icet.thogakade.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection instance;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "yash");
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        if (instance==null){
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+}
